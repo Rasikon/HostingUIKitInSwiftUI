4fb92c822fe970bffb91e1dc1791cc4813ccbd02
Ivan Redreev <redreev.ivan@gmail.com>
2020-10-28 09:35:59 +0500

    Solution

--- HostingUIKitInSwiftUI/ContentView.swift
+++ HostingUIKitInSwiftUI/ContentView.swift
@@ -16,22 +16,27 @@ struct ContentView: View {
     
     var body: some View {
         VStack(spacing: 10) {
+            
             HStack{
                 Text("Подвиньте слайдер, как можно ближе к:")
                 Text("\(lround(taskValue))")
             }
+            
             HStack {
                 Text("0")
                 CustomUISlider(value: $value, alpha: $alpha, taskValue: $taskValue, color: .red)
                 Text("100")
             }
-            Button("Проверь меня", action: {showAlert = true} ).alert(isPresented: $showAlert, content: {
-                .init(title: Text("Your score!"), message: Text("\(computeScore())"), dismissButton: .default(Text("ОК")))
-            })
+            
+            Button("Проверь меня", action: {showAlert = true})
+                .alert(isPresented: $showAlert,content: {
+                    .init(title: Text("Your score!"),
+                          message: Text("\(computeScore())"),
+                          dismissButton: .default(Text("ОК")))
+                })
+            
             Button("Начать заново", action: {
-                value = Double.random(in: 0...100)
-                taskValue = Double.random(in: 0...100)
-                alpha = Double(computeScore()) / 100
+                updateView()
             })
         }
         .padding()
@@ -40,6 +45,12 @@ struct ContentView: View {
     private func computeScore() -> Int {
         let difference = abs(lround(taskValue - value))
         return 100 - difference
+    }
+    
+    private func updateView() {
+        value = Double.random(in: 0...100)
+        taskValue = Double.random(in: 0...100)
+        alpha = Double(computeScore()) / 100
     }
 }
 
